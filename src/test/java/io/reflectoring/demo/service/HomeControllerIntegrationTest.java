abc26c1e12883485dcae1d92cfe30712743c3ff2
fanwu <wufan@163.com>
2025-07-22 06:30:20 +0800

    init master

--- src/test/java/io/reflectoring/demo/service/HomeControllerIntegrationTest.java
+++ src/test/java/io/reflectoring/demo/service/HomeControllerIntegrationTest.java
@@ -1,40 +1,44 @@
-//package io.reflectoring.demo.service;
-//
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-//import org.springframework.web.context.WebApplicationContext;
-//
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-//
-////@MySqlContainerConfig
-//public class HomeControllerIntegrationTest {
-//
-//    @Autowired
-//    private WebApplicationContext webApplicationContext;
-//
-//    private MockMvc mockMvc;
-//
-//    @BeforeEach
-//    void setup() {
-//        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-//    }
-//
-//    @Test
-//    void testHomeEndpointWithDefaultParam() throws Exception {
-//        mockMvc.perform(get("/"))
-//                .andExpect(status().isOk())
-//                .andExpect(content().string("Saved user: Test User 0"));
-//    }
-//
-//    @Test
-//    void testHomeEndpointWithCustomParam() throws Exception {
-//        mockMvc.perform(get("/").param("a", "5"))
-//                .andExpect(status().isOk())
-//                .andExpect(content().string("Saved user: Test User 5"));
-//    }
-//}
+package io.reflectoring.demo.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+//@MySqlContainerConfig
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public class HomeControllerIntegrationTest {
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    void testHomeEndpointWithDefaultParam() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Saved user: Test User 0"));
+    }
+
+    @Test
+    void testHomeEndpointWithCustomParam() throws Exception {
+        mockMvc.perform(get("/").param("a", "5"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Saved user: Test User 5"));
+    }
+}
